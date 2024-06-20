b9369782d5f0c48496c157b47ed1d4a5403252af
yuli <stremovsky@gmail.com>
2024-06-20 17:28:11 +0300

    fix all errors

--- cronjob.tf
+++ cronjob.tf
@@ -10,35 +10,73 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_cron_job" "example_cronjob" {
+resource "kubernetes_service_account" "eks_s3_access" {
+  metadata {
+    name      = "eks-s3-access"
+    namespace = "default"
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = "arn:aws:iam::452251620858:role/EKSClusterS3WriterIAMRole"
+      "kubectl.kubernetes.io/last-applied-configuration" = jsonencode({
+        apiVersion = "v1"
+        kind       = "ServiceAccount"
+        metadata = {
+          annotations = {}
+          name        = "eks-s3-access"
+          namespace   = "default"
+        }
+      })
+    }
+  }
+}
+
+resource "kubernetes_config_map" "python_script_configmap" {
+  metadata {
+    name = "python-script"
+    namespace = "default"
+  }
+  data = {
+    "cronjob.py" = file("cronjob.py")
+  }
+}
+
+resource "kubernetes_cron_job_v1" "example_cronjob" {
   metadata {
     name = "example-cronjob"
     namespace = "default"
   }
 
   spec {
-    schedule = "*/1 * * * *"  # Cron schedule (runs every minute in this example)
+    schedule  = "* * * * *"
 
     job_template {
       metadata {
-        labels = {
-          app = "example-cronjob"
-        }
+        name = "example-cronjob-job"
+        namespace = "default"
       }
       spec {
         template {
           metadata {
-            labels = {
-              app = "example-cronjob"
-            }
+            name = "example-cronjob-spec"
           }
           spec {
+            service_account_name = "eks-s3-access"
             container {
-              image = "busybox"
+              image = "python:slim"
               name  = "example-cronjob"
-              args  = ["date"]  # Command to run in the container
+              command = ["sh", "-c", "pip install requests boto3; printenv; python /code/cronjob.py"]
+              volume_mount {
+                mount_path = "/code"
+                name = kubernetes_config_map.python_script_configmap.metadata[0].name
+              }
             }
-            restart_policy = "OnFailure"
+            volume {
+              name = "python-script"
+              config_map {
+                name = kubernetes_config_map.python_script_configmap.metadata[0].name
+              }
+            }
+            restart_policy = "Never"
           }
         }
       }
