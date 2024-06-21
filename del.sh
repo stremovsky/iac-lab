e1b32f657112523d5b405862a42b3b399a300c5c
yuli <stremovsky@gmail.com>
2024-06-21 11:59:21 +0300

    fix command

--- del.sh
+++ del.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-terraform destroy
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --query 'Account' --output text`
+
+terraform destroy -var="aws_account_id=$AWS_ACCOUNT_ID"
 aws cloudformation delete-stack --stack-name demo-stack-1
